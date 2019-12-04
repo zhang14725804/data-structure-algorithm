cbf6c64561a2cdbe8c5975bcd6fe4e408f0e8736
zhang14725804 <978817036@qq.com>
2019-12-04 17:17:09 +0800

    快速排序

--- algorithm/sort.go
+++ algorithm/sort.go
@@ -3,8 +3,9 @@ package algorithm
 import "fmt"
 
 func SortMain() {
-	fmt.Println("==========")
 	origin := []int{5, 2, 7, 3, 6, 1}
-	fmt.Println(origin)
+	fmt.Println("排序之前的数据：", origin)
 	BubbleSort(origin)
+	result := QuickSort(origin)
+	fmt.Println("快速排序之后：", result)
 }
