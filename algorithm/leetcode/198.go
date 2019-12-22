4c1d00d3571a7c8e5d06a381220493c6beee9459
zhang14725804 <978817036@qq.com>
2019-12-22 13:25:51 +0800

    leetcode198-这个易懂

---
+++ algorithm/leetcode/198.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import "fmt"
+
+// Leetcode198 “House Robber”
+func Leetcode198() int {
+	/*
+		f[i]表示在前i个数中选，所有不选nums[i]的选法最大值
+		g[i]表示在前i个数中选，所有选择nums[i]的选法最大值
+
+		f[i] = max(f[i-1],g[i-1])
+		g[i] = f[i-1]+nums[i]
+	*/
+	nums := []int{1, 2, 3, 1}
+	n := len(nums)
+	f := make([]int, n+1)
+	g := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		f[i] = max(f[i-1], g[i-1])
+		g[i] = f[i-1] + nums[i-1]
+	}
+	fmt.Println(f)
+	fmt.Println(g)
+	return max(f[n], g[n])
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
-import "data-structure-algorithm/algorithm/leetcode"
-
 func main() {
 	// 测试排序
 	// sorts.SortMain()
 	// gobase.ReflectMain()
 	// gobase.ReflectStructMain()
+
 	// 测试leetcode
 	// leetcode.Letcode001()
 	// leetcode.Letcode003("abcabcbb")
@@ -17,7 +16,9 @@ func main() {
 	// leetcode.Leetcode053([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})
 	// leetcode.Leetcode120()
 	// leetcode.Leetcode063()
-	leetcode.Leetcode091()
+	// leetcode.Leetcode091()
+	// leetcode.Leetcode198()
+
 	// leetcode.Letcode001Hash()
 	// 测试数据结构
 	// linkedlist.SingleLinkedListDemo()
