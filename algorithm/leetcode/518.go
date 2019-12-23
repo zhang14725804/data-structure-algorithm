e046a0c35607d7c7c6850a207f52658a9c5e1293
zhang14725804 <978817036@qq.com>
2019-12-23 10:32:13 +0800

    leetcode518，背包问题，难理解

--- algorithm/leetcode/518.go
+++ algorithm/leetcode/518.go
@@ -1,6 +1,29 @@
 package leetcode
 
-// Leetcode518
+// Leetcode518 完全背包问题
 func Leetcode518() int {
+	/*
+		集合：所有由前i种硬币凑出的总钱数为j的凑发
+		属性：所有凑发的数量
 
+		状态计算：f[i][j]，ps：比较难理解
+
+		f[i][j] = f[i-1,j]+f[i-1,j-c],f[i-1,j-2c]+...+f[i-1,kc]
+		优化后(从二维到一维)：
+		f[i][j] = f[i-1,j]+f[i,j-c]
+		f[j] = f[j]+f[j-c]
+	*/
+	m := 5
+	coins := []int{1, 2, 5}
+
+	n := len(coins)
+	f := make([]int, m+1)
+
+	f[0] = 1
+	for i := 0; i < n; i++ {
+		for j := coins[i]; j <= m; j++ {
+			f[j] = f[j-coins[i]]
+		}
+	}
+	return f[m]
 }
