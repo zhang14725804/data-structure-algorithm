ea5c4c73d1a007166ac228f88bbcf9535421af1c
zhang14725804 <978817036@qq.com>
2019-12-25 20:03:48 +0800

    备份191225

--- algorithm/sorts/sort.go
+++ algorithm/sorts/sort.go
@@ -2,7 +2,7 @@ package sorts
 
 import "fmt"
 
-const sortMethods []string{
+var sortMethods []string{
 	"MergeSort",
 	"ShellSort",
 }
@@ -11,21 +11,22 @@ type Sort struct{
 }
 func SortMain() {
 	origin := []int{5, 2, 7, 3, 6, 1}
-	fmt.Println("排序之前的数据：", origin)
-	BubbleSort(origin)
-	BubbleSort2(origin)
-	fmt.Println("快速排序之后：", QuickSort(origin))
-	fmt.Println("快速排序==2==之后：", QuickSort2(origin))
-	InsertSort(origin)
-	InsertSort2(origin)
-	CountSort(origin, 7)
-	CountSort2(origin, 7)
-	SelectionSort(origin)
-	SelectionSort2(origin)
-	ShellSort(origin)
-	fmt.Println("归并排序之后：", MergeSort(origin))
-	// 四种排序不会
-	BucketSort(origin)
-	RadixSort(origin)
-	HeapSort(origin)
+
+	// fmt.Println("排序之前的数据：", origin)
+	// BubbleSort(origin)
+	// BubbleSort2(origin)
+	// fmt.Println("快速排序之后：", QuickSort(origin))
+	// fmt.Println("快速排序==2==之后：", QuickSort2(origin))
+	// InsertSort(origin)
+	// InsertSort2(origin)
+	// CountSort(origin, 7)
+	// CountSort2(origin, 7)
+	// SelectionSort(origin)
+	// SelectionSort2(origin)
+	// ShellSort(origin)
+	// fmt.Println("归并排序之后：", MergeSort(origin))
+	// // 四种排序不会
+	// BucketSort(origin)
+	// RadixSort(origin)
+	// HeapSort(origin)
 }
