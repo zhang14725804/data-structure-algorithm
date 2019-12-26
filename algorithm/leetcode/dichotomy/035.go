3353e6ba4459aacc9961a71d3f370d0da84ba841
zhang14725804 <978817036@qq.com>
2019-12-26 14:33:08 +0800

    leetcode35二分法

--- algorithm/leetcode/dichotomy/035.go
+++ algorithm/leetcode/dichotomy/035.go
@@ -4,8 +4,8 @@ package leetcode
 func Leetcode035() int {
 	nums := []int{1, 3, 5, 6}
 	target := 5
+	length := len(nums)
 	// 方法一 普通循环
-	// length := len(nums)
 	// for i := 0; i < length; i++ {
 	// 	if nums[i] >= target {
 	// 		return i
@@ -13,5 +13,19 @@ func Leetcode035() int {
 	// }
 	// return length
 
-	// 方法二：二分法
+	// 方法二：二分法（临界条件不好判断）
+	if length == 0 || nums[length-1] < target {
+		return length
+	}
+	l, r := 0, length-1
+	for l < r {
+		// 这里注意括号
+		mid := (l + r) >> 1
+		if nums[mid] >= target {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return r
 }
