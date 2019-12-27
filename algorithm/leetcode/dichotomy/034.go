7533615909ce87c5a16d2a1064c3edb509bf6a2d
zhang14725804 <978817036@qq.com>
2019-12-27 09:35:24 +0800

    leetcode34-常规方法一些注释

--- algorithm/leetcode/dichotomy/034.go
+++ algorithm/leetcode/dichotomy/034.go
@@ -5,6 +5,7 @@ func Leetcode034() []int {
 	nums := []int{5, 7, 7, 8, 8, 10}
 	target := 6
 	n := len(nums)
+	// 常规map方法
 	mp := make(map[int][]int)
 	for i := 0; i < n; i++ {
 		if arr, ok := mp[nums[i]]; ok {
@@ -15,10 +16,14 @@ func Leetcode034() []int {
 		}
 	}
 	res, _ := mp[target]
+	// 判空
 	if len(res) == 0 {
 		return []int{-1, -1}
 	}
 	l := res[0]
 	r := res[len(res)-1]
 	return []int{l, r}
+
+	// 二分法（两次二分法）
+
 }
