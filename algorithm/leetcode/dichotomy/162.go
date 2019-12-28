1ba83d6d1e7e48a6cc4d3cd3b626137f27a77a1b
zhang14725804 <978817036@qq.com>
2019-12-28 11:23:11 +0800

    leetcode162-二分法

--- algorithm/leetcode/dichotomy/162.go
+++ algorithm/leetcode/dichotomy/162.go
@@ -1,6 +1,18 @@
 package leetcode
 
-// Leetcode162  Find Peak Element (二分法)
+// Leetcode162  Find Peak Element（相邻两个值不同，可以二分）有难度 (二分法)
 func Leetcode162(nums []int) int {
-
+	/*
+		出现拐点，这个点就是峰值。不出现拐点，边界就是峰值
+	*/
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := (left + right) >> 1
+		if nums[mid] > nums[mid+1] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return right
 }
