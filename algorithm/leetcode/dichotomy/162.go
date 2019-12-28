225ffacdb3ada530df93118e0bbbe1772ebb7bae
zhang14725804 <978817036@qq.com>
2019-12-28 11:40:53 +0800

    leetcode162-二分法

--- algorithm/leetcode/dichotomy/162.go
+++ algorithm/leetcode/dichotomy/162.go
@@ -3,6 +3,7 @@ package leetcode
 // Leetcode162  Find Peak Element（相邻两个值不同，可以二分）有难度 (二分法)
 func Leetcode162(nums []int) int {
 	/*
+		此题没有两段性的性质，但是可以每次把答案的范围缩小一半
 		出现拐点，这个点就是峰值。不出现拐点，边界就是峰值
 		为什么不需要判断边界（for循环已经处理了）
 	*/
