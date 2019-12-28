4274258af3ddda6ef56de78c23daf77cdc2fc833
zhang14725804 <978817036@qq.com>
2019-12-28 12:28:50 +0800

    leetcode287-二分法，难理解

--- algorithm/leetcode/dichotomy/287.go
+++ algorithm/leetcode/dichotomy/287.go
@@ -1,6 +1,6 @@
 package leetcode
 
-// Leetcode287  Find the Duplicate Number 有难度 (二分法)
+// Leetcode287  Find the Duplicate Number (二分法)
 func Leetcode287() int {
 	nums := []int{1, 3, 4, 2, 2}
 	/*
@@ -12,6 +12,7 @@ func Leetcode287() int {
 	for left < right {
 		mid := (left + right) >> 1
 		cnt := 0
+		// 判断难理解
 		for _, x := range nums {
 			if x >= left && x <= mid {
 				cnt++
