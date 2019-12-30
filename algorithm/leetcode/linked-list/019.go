417b6b8b26b4474e28aa2618357f56c7cb12daf6
zhang14725804 <978817036@qq.com>
2019-12-30 10:38:13 +0800

    leetcode019-指针，快慢指针，不懂

--- algorithm/leetcode/linked-list/019.go
+++ algorithm/leetcode/linked-list/019.go
@@ -1,7 +1,12 @@
 package leetcode
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 // Leetcode019 Remove Nth Node From End of List
-func Leetcode019() {
+func Leetcode019(head *ListNode, n int) *ListNode {
 	/*
 		单链表，每个节点由data和指向下一个节点的指针组成
 
@@ -9,5 +14,24 @@ func Leetcode019() {
 		（2）让某个指针（假设first）向后走n步
 		（3）first，second两个指针同时走，相差n步，当first走到结尾时，终止。second走到了倒数第n+1个节点
 		（4）倒数n+1个节点直接指向n的下一个节点就好了
+
+		todos：：指针这里还是比较绕，难懂...这一堆都不知道干了些什么！！！！
 	*/
+	var fast, slow *ListNode
+	fast = head
+	slow = head
+
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	if fast == nil {
+		head = head.Next
+		return head
+	}
+	for fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return head
 }
