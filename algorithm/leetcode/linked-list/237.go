bee4bb93b1fa9dc8744c1acd2639dd6ee4f39c3a
zhang14725804 <978817036@qq.com>
2020-01-02 10:46:03 +0800

    leetcode237-方法二，优秀的思路

--- algorithm/leetcode/linked-list/237.go
+++ algorithm/leetcode/linked-list/237.go
@@ -10,6 +10,9 @@ func Leetcode237(node *ListNode) {
 	/*
 		让下个节点的val覆盖当前节点的val，然后删除下个节点，让本届点的next指向下个节点的下个节点
 	*/
-	node.Val = node.Next.Val
-	node.Next = node.Next.Next
+	// 方法1
+	// node.Val = node.Next.Val
+	// node.Next = node.Next.Next
+	// 方法2
+	*node = *(node.Next)
 }
