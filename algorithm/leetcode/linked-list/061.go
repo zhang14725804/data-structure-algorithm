7674dce2bfd31641c358264d434aa49b4ee9f850
zhang14725804 <978817036@qq.com>
2020-02-07 22:41:50 +0800

    leetcode061

---
+++ algorithm/leetcode/linked-list/061.go
@@ -0,0 +1,11 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// Leetcode061 Rotate List,注意理解，每翻转一次，把最后一个数字往前移动一次，由末尾移到首位
+func Leetcode061(head *ListNode) *ListNode {
+
+}
