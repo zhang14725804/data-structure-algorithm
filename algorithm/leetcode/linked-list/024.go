8d85b0e06b7f6db2095c96e57cdb77ddb87a5efc
zhang14725804 <978817036@qq.com>
2020-02-08 20:43:41 +0800

    leetcode24

---
+++ algorithm/leetcode/linked-list/024.go
@@ -0,0 +1,32 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/*
+	24.Swap Nodes in Pairs
+
+	p.Next = b
+	a.Next = b.Next
+	b.Next = a
+	p = a
+*/
+func Leetcode024(head *ListNode) *ListNode {
+	// 虚拟头结点
+	newHead := &ListNode{}
+	newHead.Next = head
+
+	p := newHead
+	for p.Next != nil && p.Next.Next != nil {
+		a := p.Next
+		b := p.Next.Next
+
+		p.Next = b
+		a.Next = b.Next
+		b.Next = a
+		p = a
+	}
+	return newHead.Next
+}
