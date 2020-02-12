d4e42e9e8508c5900dec5922f57115578e5944be
zhang14725804 <978817036@qq.com>
2020-02-12 09:41:38 +0800

    leetcode148-非常规排序-复杂

---
+++ algorithm/leetcode/linked-list/148.go
@@ -0,0 +1,15 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/*
+	148. Sort List 
+
+	Sort a linked list in O(n log n) time using constant space complexity.
+*/
+func sortList(head *ListNode) *ListNode {
+    
+}
