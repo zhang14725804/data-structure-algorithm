739d90ed9fd8d1f2eff7e8350ef6d54ff195e88f
zhang14725804 <978817036@qq.com>
2020-02-13 09:46:20 +0800

    树专题

---
+++ algorithm/leetcode/tree/098.go
@@ -0,0 +1,15 @@
+package leetcode
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+/**
+	98. Validate Binary Search Tree
+
+	自上而下递归判断
+*/
+ func isValidBST(root *TreeNode) bool {
+    
+}
