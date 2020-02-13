96b7da84c3bf8f939ca427df4403a21d7709c25a
zhang14725804 <978817036@qq.com>
2020-02-13 10:42:19 +0800

    leetcode094

---
+++ algorithm/leetcode/tree/094.go
@@ -0,0 +1,13 @@
+package leetcode
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+/**
+	94. Binary Tree Inorder Traversal
+*/
+func inorderTraversal(root *TreeNode) []int {
+    
+}
