b5cc3688520846d6a5cf331a838d5dec2c04e1a8
zhang14725804 <978817036@qq.com>
2020-02-13 10:27:18 +0800

    leetcode098-判断是否是二叉搜索树

--- algorithm/leetcode/tree/098.go
+++ algorithm/leetcode/tree/098.go
@@ -9,7 +9,24 @@ type TreeNode struct {
 	98. Validate Binary Search Tree
 
 	自上而下递归判断
+	
+	https://www.cnblogs.com/coffeeSS/p/5452719.html
 */
- func isValidBST(root *TreeNode) bool {
-    
+func isValidBST(root *TreeNode) bool {
+	const MaxUint = ^uint(0) 
+	const MinUint = 0 
+	const MaxInt = int(MaxUint >> 1) 
+	const MinInt = -MaxInt - 1
+    return dfs(root,MinInt,MaxInt)
+}
+
+func dfs(root *TreeNode,minv,maxv int) bool{
+	if root==nil{
+		return true
+	}
+	if root.Val < minv || root.Val>maxv{
+		return false
+	}
+
+	return dfs(root.Left,minv,root.Val-1) && dfs(root.Right,root.Val+1,maxv)
 }
