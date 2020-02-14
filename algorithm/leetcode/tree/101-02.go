9204fec360aad764234bad9a6982a701a3017dec
zhang14725804 <978817036@qq.com>
2020-02-14 14:06:07 +0800

    leetcode101-迭代的方式有问题

--- algorithm/leetcode/tree/101-02.go
+++ algorithm/leetcode/tree/101-02.go
@@ -1,30 +1,69 @@
+
 package leetcode
-
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
 /**
 	101. Symmetric Tree
 	判断镜像二叉树问题
 
-	迭代的方式（不同的遍历方式，然后比较节点即可）
-	左边：左中右
-	右边：右中左
+	迭代（bfs）的方式（不同的遍历方式，然后比较节点即可）
+	左边：左中右遍历
+	右边：右中左遍历
 */
 func isSymmetric(root *TreeNode) bool {
     if root==nil{
 		return true
 	}
-	return dfs(root.Left,root.Right)
-}
+	var left Stack
+	var right Stack
 
-func dfs(left,right *TreeNode) bool{
-	if left==nil || right==nil{
-		return left==nil && right==nil
+	l:=root.Left
+	r:=root.Right
+
+	for left.size()>0 || right.size()>0 || r!=nil || l !=nil{
+		for r!=nil && l !=nil {
+			left.push(l)
+			l = left.Left
+			right.push(r)
+			r = right.Right
+		}
+
+		if l ==nil || r==nil{
+			return false
+		}
+
+		l = left.pop()
+		r = right.pop()
+		if l.Val != r.Val{
+			return false
+		}
+
+		// 这里注意
+		l = l.Right
+		r = r.Left
 	}
-	return left.Val == right.Val && dfs(left.Left,right.Right) && dfs(left.Right,right.Left)
+	return true
 }
 
+// 利用golang slice实现栈
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+type Stack []TreeNode
+
+func (stack Stack) size() int {
+    return len(stack)
+}
+func (s *Stack) push(node TreeNode){
+	*s = append(*s, node)
+}
+
+func (s *Stack) pop() interface{}{
+	theStack:=*s
+	if len(theStack) == 0 {
+		return nil
+	}
+	node := theStack[len(theStack)-1]
+	*s = theStack[0 : len(theStack)-1]
+	return node
+}
