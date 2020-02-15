7eae32f0a54f2997262cb6da180f71d104f83c51
zhang14725804 <978817036@qq.com>
2020-02-15 13:28:33 +0800

    leetcode105-思路不错

--- algorithm/leetcode/tree/105.go
+++ algorithm/leetcode/tree/105.go
@@ -1,33 +1,39 @@
 package leetcode
 
 type TreeNode struct {
-	Val int
-	Left *TreeNode
+	Val   int
+	Left  *TreeNode
 	Right *TreeNode
 }
 
 /*
 	105. Construct Binary Tree from Preorder(前序遍历) and Inorder(中序遍历) Traversal
 */
-var pos map[int]int
+
+/*
+	var pos map[int]int
+	这样写会报错：assignment to entry in nil map
+*/
+var pos = make(map[int]int)
+
 func buildTree(preorder []int, inorder []int) *TreeNode {
-	n:=len(preorder)
-	for i:=0;i<n;i++{
+	n := len(preorder)
+	for i := 0; i < n; i++ {
 		pos[inorder[i]] = i
-	}	
-	return dfs(preorder,inorder,0,n-1,0,n-1)
+	}
+	return dfs105(preorder, inorder, 0, n-1, 0, n-1)
 }
 
 // 递归函数
-func dfs(preorder,inorder []int,pl,pr,il,ir int) *TreeNode{
-	if(pl > pr){
+func dfs105(preorder, inorder []int, pl, pr, il, ir int) *TreeNode {
+	if pl > pr {
 		return nil
 	}
-	val:=preorder[pl]
-	k:=pos[val]
-	length:=k-il
-	root:=&TreeNode{val,nil,nil}
-	root.Left = dfs(preorder,inorder,pl+1,pl+length,il,k-1)
-	root.Right = dfs(preorder,inorder,pl+length+1,pr,k+1,ir)
+	val := preorder[pl]
+	k := pos[val]
+	length := k - il
+	root := &TreeNode{val, nil, nil}
+	root.Left = dfs105(preorder, inorder, pl+1, pl+length, il, k-1)
+	root.Right = dfs105(preorder, inorder, pl+length+1, pr, k+1, ir)
 	return root
 }
