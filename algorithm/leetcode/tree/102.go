2b9f76371f4c89cdafdc368a45558c21356f4022
zhang14725804 <978817036@qq.com>
2020-02-15 13:34:14 +0800

    leetcode105思路和102

---
+++ algorithm/leetcode/tree/102.go
@@ -0,0 +1,14 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+/*
+	102. Binary Tree Level Order Traversal
+	层序遍历
+*/
+func levelOrder(root *TreeNode) [][]int {
+    
+}
