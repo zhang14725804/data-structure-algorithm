6f285cf37e61222ae1601e21039c916a2d5a8d8b
zhang14725804 <978817036@qq.com>
2020-02-16 12:16:35 +0800

    leetcode102-思路不错

--- algorithm/leetcode/tree/102.go
+++ algorithm/leetcode/tree/102.go
@@ -5,6 +5,7 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
 /*
 	102. Binary Tree Level Order Traversal
 	层序遍历，以层为单位做dfs。以队列的方式存储每一层，和101-02栈的思路一样
@@ -12,41 +13,43 @@ type TreeNode struct {
 func levelOrder(root *TreeNode) [][]int {
 	// res:=make([][]int)
 	// 声明二维数组（上面的错了）
-    var res [][]int
-	if root==nil{
+	var res [][]int
+	if root == nil {
 		return res
 	}
 
 	var q Queue
 	q.push(*root)
-	for len(q)>0{
-		length:=len(q)
+	for len(q) > 0 {
+		length := len(q)
 		//level:=make([]int)
 		// 声明二维数组（上面的错了）
-        var level []int
-		for i:=0;i<length;i++{
-			t:=q.front()
-			level = append(level,t.Val)
-			if t.Left!=nil{
+		var level []int
+		for i := 0; i < length; i++ {
+			t := q.front()
+			level = append(level, t.Val)
+			if t.Left != nil {
 				// 注意是指针
 				q.push(*t.Left)
 			}
-			if t.Right!=nil{
+			if t.Right != nil {
 				// 注意是指针
 				q.push(*t.Right)
 			}
 		}
-		res = append(res,level)
+		res = append(res, level)
 	}
 	return res
 }
 
 // 利用slice实现队列
 type Queue []TreeNode
+
 // 入队
 func (s *Queue) push(node TreeNode) {
 	*s = append(*s, node)
 }
+
 // 出队
 func (s *Queue) front() *TreeNode {
 	theStack := *s
@@ -55,6 +58,6 @@ func (s *Queue) front() *TreeNode {
 		return node
 	}
 	node = &theStack[0]
-	*s = theStack[1 : len(theStack)]
+	*s = theStack[1:len(theStack)]
 	return node
 }
