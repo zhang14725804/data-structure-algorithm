085b01e496005e6f7af4b538dafc3fa7650af34e
zhang14725804 <978817036@qq.com>
2020-02-17 09:24:12 +0800

    leetcode297，目前不支持golang语言

---
+++ algorithm/leetcode/tree/297.go
@@ -0,0 +1,15 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/*
+	297. Serialize and Deserialize Binary Tree(序列化和反序列化)
+	todos：：20200217，目前不支持golang
+
+	只有前序遍历，并不能唯一确定一棵二叉树（但是如果加上空节点，就可以确定）
+	前序遍历加中序遍历，就可以唯一确定一棵二叉树
+*/
