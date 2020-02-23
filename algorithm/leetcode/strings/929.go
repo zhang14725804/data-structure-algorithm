bdb83f9c06671d878bd63ff73b553fbc97f62494
zhang14725804 <978817036@qq.com>
2020-02-23 18:13:18 +0800

    929简单注释

--- algorithm/leetcode/strings/929.go
+++ algorithm/leetcode/strings/929.go
@@ -9,6 +9,7 @@ func numUniqueEmails(emails []string) int {
 		email:=[]rune(e)
 		at:= find(email,'@')
 		var name string
+		// 排除”+“和”.“，生成name
 		for _,c :=range(email[:at]){
 			if c == '+' {
 				break
@@ -18,20 +19,22 @@ func numUniqueEmails(emails []string) int {
 		}
 
 		domain:=email[at+1:]
+		// 每个email存入hash表
 		emain :=name + "@" + string(domain) 
 		hash[emain] = 1
 	}
+	// 统计hash表长度（办法好像有点蠢）
 	res:=0
 	for _,_=range hash{
 		res+=1
 	}
 	return res
 }
+// rune中查找元素返回元素下标
 func find(arr []rune, char rune) int{
 	for i:=0;i<len(arr);i++{
 		// 注意这里
-		a := arr[i]
-		if  a== char{
+		if  arr[i] == char{
 			return i
 		}
 	}
