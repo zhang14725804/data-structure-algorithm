617001bc7803637ee298fd1becf250d1002a3447
zhang14725804 <978817036@qq.com>
2020-03-09 09:17:55 +0800

    706-涉及hash表

---
+++ algorithm/leetcode/base-data-structure/706.go
@@ -0,0 +1,42 @@
+/*
+	706. Design HashMap
+
+	hash两种做法：开放寻址法，拉链法
+*/
+
+type MyHashMap struct {
+    
+}
+
+
+/** Initialize your data structure here. */
+func Constructor() MyHashMap {
+    
+}
+
+
+/** value will always be non-negative. */
+func (this *MyHashMap) Put(key int, value int)  {
+    
+}
+
+
+/** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+func (this *MyHashMap) Get(key int) int {
+    
+}
+
+
+/** Removes the mapping of the specified value key if this map contains a mapping for the key */
+func (this *MyHashMap) Remove(key int)  {
+    
+}
+
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Put(key,value);
+ * param_2 := obj.Get(key);
+ * obj.Remove(key);
+ */
