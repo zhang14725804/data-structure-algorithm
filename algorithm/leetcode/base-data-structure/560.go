10de63a0ef29387946e50a72aae50a0904614b52
zhang14725804 <978817036@qq.com>
2020-03-11 09:26:36 +0800

    560如果变换条件如何作答

--- algorithm/leetcode/base-data-structure/560.go
+++ algorithm/leetcode/base-data-structure/560.go
@@ -2,17 +2,19 @@
 	560. Subarray Sum Equals K
 
 	考察的算法：前缀和和hash（边界问题）
+
+	把这个题改成多少和小于等于K：：平衡树的基础上二分
 */
 func subarraySum(nums []int, k int) int {
 	hash := make(map[int]int,0)
 	// 注意边界问题
 	hash[0] = 1
 	res := 0
-	for i,sum:=0,0;i<len(nums);i++{
+	for i,sum := 0,0;i < len(nums); i++{
 		// 前缀和
 		sum += nums[i]
-		res+=hash[sum-k]
-		hash[sum]+=1
+		res += hash[sum-k]
+		hash[sum] += 1
 	}
 	return res
 }
