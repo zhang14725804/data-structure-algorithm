fc41101a81010b7581f3d1f8c6973d24ce671fb1
zhang14725804 <978817036@qq.com>
2020-03-12 16:28:39 +0800

    实现堆

--- algorithm/leetcode/base-data-structure/692.go
+++ algorithm/leetcode/base-data-structure/692.go
@@ -15,3 +15,60 @@
 	（todos：：归并排序，快排要看看）
 */
 
+/*
+	实现最小堆参考（https://yangjiahao106.github.io/2019/01/15/golang-%E6%9C%80%E5%A4%A7%E5%A0%86%E5%92%8C%E6%9C%80%E5%B0%8F%E5%A0%86/）
+*/
+type minHeap struct{
+	Element []int
+}
+// 数组中第一个元素不使用，存放一个小于堆中任何数字的值用于结束循环
+func newMinHeap() *minHeap{
+	return &minHeap{Element:[]int{math.MinInt64}}
+}
+// 插入元素就直接将元素增加到堆的末尾，然后进行上浮操作，使二叉堆有序。
+func (h *minHeap) push(v int){
+	h.Element = append(h.Element, v)
+	n := len(h.Element) - 1
+	// 上浮
+	for ;h.Element[n/2] > v;n /= 2{
+		h.Element[n] = h.Element[n/2]
+	}
+
+	h.Element[n] = v
+}
+
+/*
+	删除最小值
+	删除最大元素就直接从二叉堆顶端删除，然后进行下沉操作
+*/ 
+func (h *minHeap) pop() int{
+	if len(h.Element) <= 1 {
+		return 0
+	}
+	min := h.Element[1]
+	last := h.Element[len(h.Element)-1]
+
+	var i,child int
+	for i = 1;i*2<len(h.Element);i = child {
+		child = i*2
+		if child < len(h.Element) -1 && h.Element[child+1] < h.Element[child]{
+			child += 1
+		}
+		if last > h.Element[child]{
+			h.Element[i] = h.Element[child]
+		}else{
+			break
+		}
+	}
+	h.Element[i] = last
+	h.Element = h.Element[:len(h.Element)-1]	
+	return min
+}
+
+// 获取最小值
+func (h *minHeap) mix() int{
+	if len(h.Element) > 1{
+		return h.Element[1]
+	}
+	return 0
+}
