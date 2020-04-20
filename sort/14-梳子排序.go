92875b8b60dd5b0d15bf6568ecd52ac3c0b38e86
zhang14725804 <978817036@qq.com>
2020-04-20 11:43:02 +0800

    侏儒排序，休眠排序，木桶排序，梳子排序

---
+++ sort/14-梳子排序.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/*
+	梳子排序：希尔排序的最优版
+*/
+
+func ComoSort(arr []int) []int {
+	l := len(arr)
+	gap := l
+	for gap>1{
+		// 为什么这么做
+		gap = gap*10/13
+		// 收缩步长交换位置
+		for i:=0;i+gap<l;i++{
+			if arr[i]>arr[i+gap]{
+				arr[i],arr[i+gap] = arr[i+gap],arr[i]
+			}
+		}
+	}
+	return arr
+}
+
+func mainComoSort()  {
+	array := []int{16,8,1,24,30}
+	fmt.Println(ComoSort(array))
+}
--- sort/9-shellSort.go
+++ sort/9-shellSort.go
@@ -1,43 +1,44 @@
 package main
 
 import "fmt"
+
 /*
 	希尔排序（多线程排序）
 	不断地收缩步长
 */
-func ShellSort(arr []int) []int  {
+func ShellSort(arr []int) []int {
 	l := len(arr)
-	if l<=1{
+	if l <= 1 {
 		return arr
-	}else{
-		gap := l/2
+	} else {
+		gap := l / 2
 		for gap > 0 {
 			//
-			for i:=0;i<gap;i++{
-				step(arr,i,gap)
+			for i := 0; i < gap; i++ {
+				step(arr, i, gap)
 			}
-			gap/=2 // gap--
+			gap /= 2 // gap--
 		}
 	}
 	return arr
 }
 
 // 每个步长在进行插入排序
-func step(arr []int, start,gap int)  {
+func step(arr []int, start, gap int) {
 	l := len(arr)
-	for i:=start+gap;i<l;i+=gap{
+	for i := start + gap; i < l; i += gap {
 		// todo：不太懂
 		backup := arr[i] // 备份插入的数据
-		j:=i-gap // 上一个位置循环找到位置插入
-		for j>=0 && backup<arr[j]{
+		j := i - gap     // 上一个位置循环找到位置插入
+		for j >= 0 && backup < arr[j] {
 			arr[j+gap] = arr[j] // 从前往后移动
-			j-=gap
+			j -= gap
 		}
 		// 插入
 		arr[j+gap] = backup
 	}
 }
-func main()  {
-	arr:=[]int{9,2,5,0,6,1,7,8,4,3}
+func main() {
+	arr := []int{9, 2, 5, 0, 6, 1, 7, 8, 4, 3}
 	fmt.Println(ShellSort(arr))
 }
