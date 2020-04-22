4f830fa6c5d6d2203d4f8a91e82de81efbe30f53
zhang14725804 <978817036@qq.com>
2020-04-22 11:25:38 +0800

    leetcode-1362

---
+++ algorithm/leetcode/177周赛/1362.go
@@ -0,0 +1,14 @@
+/*
+	平方根，思路不错
+*/
+func closestDivisors(num int) []int {
+	for i:=int(math.Sqrt(float64(num+2)));i>0;i--{
+		if (num+1) % i == 0{
+			return []int{i,(num+1)/i}
+		}
+		if (num+2) % i == 0{
+			return []int{i,(num+2)/i}
+		}
+	}
+	return []int{0,0}
+}
