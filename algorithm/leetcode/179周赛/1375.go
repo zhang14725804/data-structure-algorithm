063ffa76f90486ff174f5212e5c622e6869e47e4
zhang14725804 <978817036@qq.com>
2020-04-24 09:20:58 +0800

    leetcode-1375

--- algorithm/leetcode/179周赛/1375.go
+++ algorithm/leetcode/179周赛/1375.go
@@ -1,3 +1,20 @@
 /*
-
+	todo:题意比较抽象😅，思路妙
 */
+func numTimesAllBlue(light []int) int {
+	// res答案，k最大值
+	res,k := 0,0
+	for i := 0; i < len(light); i++ {
+		k = max(k,light[i])
+		if k==i+1{
+			res++
+		}
+	}
+	return res
+}
+func max(x,y int) int{
+	if x>y{
+		return x
+	}
+	return y
+}
