65af7f448a5b607347cb4d3f2515f32a81d06e41
zhang14725804 <978817036@qq.com>
2020-04-26 09:32:32 +0800

    剑指offer17

---
+++ algorithm/剑指offer/017.go
@@ -0,0 +1,16 @@
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+func printListReversingly(head *ListNode) []int {
+	res := make([]int,0)
+	for head!=nil{
+		res = append(res,head.Val)
+		head = head.Next
+	}
+	// 数组反转
+	for i,j:=0,len(res)-1;i<j;i,j = i+1,j-1{
+		res[i],res[j] = res[j],res[i]
+	}
+	return res
+}
