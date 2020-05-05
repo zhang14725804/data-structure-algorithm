b48c835f4a82f0691a749fbf70803c17c6508c71
zhang14725804 <978817036@qq.com>
2020-05-05 08:06:53 +0800

    对称二叉树

---
+++ algorithm/剑指offer/038.go
@@ -0,0 +1,20 @@
+/*
+	输入一个二叉树，将它变换为它的镜像。
+*/
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+// 思路：所有左右子树互换位置
+func mirror(root *TreeNode)  {
+    if root == nil {
+		return
+	}
+	mirror(root.Left)
+	mirror(root.Right)
+	swap(root)
+}
+func swap(root *TreeNode){
+	root.Left,root.Right = root.Right,root.Left
+}
