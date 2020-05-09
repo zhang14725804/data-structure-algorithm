24d1fd6b35555a3c9314c5c8119ba7d62d6b18e4
zhang14725804 <978817036@qq.com>
2020-05-09 18:48:29 +0800

    最低公共祖先

---
+++ algorithm/剑指offer/088.go
@@ -0,0 +1,30 @@
+/*
+	给出一个二叉树，输入两个树节点，求它们的最低公共祖先。
+
+	一个树节点的祖先节点包括它本身。
+
+	递归方法
+*/
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+func lowestCommonAncestor(root *TreeNode, p *TreeNode, q *TreeNode) *TreeNode {
+	if root == nil{
+		return nil
+	}
+	if root == q || root == p{
+		return root
+	}
+	left:=lowestCommonAncestor(root.Left,p,q)
+	right:=lowestCommonAncestor(root.Right,p,q)
+	
+	if left !=nil && right!=nil{
+		return root
+	}
+	if left !=nil{
+		return left
+	}
+	return right
+}
