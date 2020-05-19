3723e3b85a36a4691ba7e11fb567bed8f1842810
zhang14725804 <978817036@qq.com>
2020-05-19 10:37:06 +0800

    双链表操作

--- data-structures/double_link/double_link.go
+++ data-structures/double_link/double_link.go
@@ -2,37 +2,37 @@ package double_link
 
 import "fmt"
 
-// 双链表结构体
+// 双链表结构体（适合插入删除操作比较多的数据）
 type DoubleLink struct {
-	Head *DoubleLinkNode
+	Head   *DoubleLinkNode
 	Length int
 }
 
-func NewDoubleLink() *DoubleLink  {
+func NewDoubleLink() *DoubleLink {
 	return &DoubleLink{
 		Head:   NewDoublelinkNode(nil),
 		Length: 0,
 	}
 }
 
-func (list *DoubleLink) Getlength() int  {
+func (list *DoubleLink) Getlength() int {
 	return list.Length
 }
 
-func (list *DoubleLink) GetFirstNode() *DoubleLinkNode  {
+func (list *DoubleLink) GetFirstNode() *DoubleLinkNode {
 	return list.Head.Next
 }
 
 // 头插入
-func (list *DoubleLink) InsertBefore(node *DoubleLinkNode){
+func (list *DoubleLink) InsertBefore(node *DoubleLinkNode) {
 
-	head:=list.Head
+	head := list.Head
 	//
-	if head.Next == nil{
+	if head.Next == nil {
 		node.Next = nil
 		head.Next = node
 		node.Prev = head
-	}else{
+	} else {
 		head.Next.Prev = node
 		node.Next = head.Next
 		head.Next = node
@@ -42,16 +42,16 @@ func (list *DoubleLink) InsertBefore(node *DoubleLinkNode){
 }
 
 // 尾插入
-func (list *DoubleLink) InsertAtfer(node *DoubleLinkNode){
-	head:=list.Head
+func (list *DoubleLink) InsertAtfer(node *DoubleLinkNode) {
+	head := list.Head
 	//
-	if head.Next == nil{
+	if head.Next == nil {
 		node.Next = nil
 		head.Next = node
 		node.Prev = head
-	}else{
+	} else {
 		// 循环找到最后一个
-		for head.Next!=nil{
+		for head.Next != nil {
 			head = head.Next
 		}
 		head.Next = node
@@ -61,36 +61,36 @@ func (list *DoubleLink) InsertAtfer(node *DoubleLinkNode){
 }
 
 // 序列化
-func (list *DoubleLink) String() string{
+func (list *DoubleLink) String() string {
 	var str1 string
 	var str2 string
-	head:=list.Head
-	for head.Next!=nil{
-		str1+=fmt.Sprintf("%v-->",head.Next.Value)
+	head := list.Head
+	for head.Next != nil {
+		str1 += fmt.Sprintf("%v-->", head.Next.Value)
 		head = head.Next
 	}
-	str1+=fmt.Sprintf("nil")
-	str1+="\n"
+	str1 += fmt.Sprintf("nil")
+	str1 += "\n"
 	//
-	for head!=list.Head{
-		str2+=fmt.Sprintf("<--%v",head.Value)
+	for head != list.Head {
+		str2 += fmt.Sprintf("<--%v", head.Value)
 		head = head.Prev
 	}
-	str1+=fmt.Sprintf("nil")
-	str2+="\n"
-	return str1+str2
+	str1 += fmt.Sprintf("nil")
+	str2 += "\n"
+	return str1 + str2
 }
 
 // 节点前插入
 func (list *DoubleLink) InsertNodeAfter(dest *DoubleLinkNode, node *DoubleLinkNode) bool {
 	p := list.Head
 	// 循环遍历链表
-	for p.Next != nil && p.Next != dest{
+	for p.Next != nil && p.Next != dest {
 		p = p.Next
 	}
 
-	if p.Next == dest{
-		if p.Next.Next !=nil{
+	if p.Next == dest {
+		if p.Next.Next != nil {
 			p.Next.Next.Prev = node
 		}
 		node.Next = p.Next.Next
@@ -98,21 +98,21 @@ func (list *DoubleLink) InsertNodeAfter(dest *DoubleLinkNode, node *DoubleLinkNo
 		node.Prev = p.Next
 		list.Length++
 		return true
-	}else{
+	} else {
 		return false
 	}
 }
 
 // 节点后插入
-func (list *DoubleLink) InsertNodeBefore(dest *DoubleLinkNode, node *DoubleLinkNode) bool{
+func (list *DoubleLink) InsertNodeBefore(dest *DoubleLinkNode, node *DoubleLinkNode) bool {
 	p := list.Head
 	// 循环遍历链表
-	for p.Next != nil && p.Next != dest{
+	for p.Next != nil && p.Next != dest {
 		p = p.Next
 	}
 
-	if p.Next == dest{
-		if p.Next!=nil{
+	if p.Next == dest {
+		if p.Next != nil {
 			p.Next.Prev = node
 		}
 		node.Next = p.Next
@@ -120,7 +120,59 @@ func (list *DoubleLink) InsertNodeBefore(dest *DoubleLinkNode, node *DoubleLinkN
 		p.Next = node
 		list.Length++
 		return true
-	}else{
+	} else {
 		return false
 	}
+}
+
+// 根据index位置获取节点
+func (list *DoubleLink) GetNodeAtIndex(index int) *DoubleLinkNode {
+	if index > list.Length-1 || index < 0 {
+		return nil
+	}
+	head := list.Head
+	//
+	for index > -1 {
+		head = head.Next
+		index--
+	}
+	return head
+}
+
+// 删除节点
+func (list *DoubleLink) DeleteNode(node *DoubleLinkNode) bool {
+	if node == nil {
+		return false
+	}
+	p := list.Head
+	for p.Next != nil && p.Next != node {
+		p = p.Next
+	}
+	if p.Next == node {
+		if p.Next.Next != nil {
+			p.Next.Next.Prev = p
+		}
+		p.Next = p.Next.Next
+		list.Length--
+		return true
+	}
+	return false
+}
+
+func (list *DoubleLink) DeleteNodeAtIndex(index int) bool {
+	if index > list.Length-1 || index < 0 {
+		return false
+	}
+	p := list.Head
+	//
+	for index > 0 {
+		p = p.Next
+		index--
+	}
+	if p.Next.Next != nil {
+		p.Next.Next.Prev = p
+	}
+	p.Next = p.Next.Next
+	list.Length--
+	return true
 }
