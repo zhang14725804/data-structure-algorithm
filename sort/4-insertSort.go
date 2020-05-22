480c6d68d8f3ccab449720cda706cc5d11d872bb
zhang14725804 <978817036@qq.com>
2020-05-22 15:02:22 +0800

    插入排序

--- sort/4-insertSort.go
+++ sort/4-insertSort.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 /*
-	插入排序（略微有点绕，有点绕）
+	插入排序
 	从左往右，从右往左两种
 */
-func InsertSort(arr []int) []int{
+func InsertSort1(arr []int) []int {
 	l := len(arr)
-	if l<=1{
+	if l <= 1 {
 		return arr
 	}
 	// 跳过第一个
-	for i:=1;i<l;i++{
+	for i := 1; i < l; i++ {
 		backup := arr[i] // 备份插入的数据
-		j:=i-1 // 上一个位置循环找到位置插入
-		for j>=0 && backup<arr[j]{
+		j := i - 1       // 上一个位置循环找到位置插入
+		for j >= 0 && backup < arr[j] {
 			arr[j+1] = arr[j] // 从前往后移动
 			j--
 		}
@@ -25,7 +25,23 @@ func InsertSort(arr []int) []int{
 	return arr
 }
 
-func mainInsert()  {
-	arr:=[]int{9,2,5,0,6,1,7,8,4,3}
-	fmt.Println(InsertSort(arr))
+func InsertSort2(array []int) []int {
+	length := len(array)
+	if length < 2 {
+		return array
+	}
+	for i := 1; i < length; i++ {
+		// 注意：j >= 0
+		for j := i - 1; j >= 0; j-- {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+	return array
+}
+
+func mainInsert() {
+	arr := []int{9, 2, 5, 0, 6, 1, 7, 8, 4, 3}
+	fmt.Println(InsertSort1(arr))
 }
