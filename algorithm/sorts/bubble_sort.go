690e2614d0e6970d695f6824b9e4d25053502bc7
zhang14725804 <978817036@qq.com>
2020-05-22 11:49:56 +0800

    冒泡排序

--- algorithm/sorts/bubble_sort.go
+++ algorithm/sorts/bubble_sort.go
@@ -2,16 +2,17 @@ package sorts
 
 import "fmt"
 
-// BubbleSort 冒泡排序思想：
 /*
+	冒泡排序：
+
 	从尾部开始比较相邻的两个元素，如果尾部的元素比前面的大，就交换两个元素的位置。
 	往前对每个相邻的元素都做这样的比较、交换操作，这样到数组头部时，第 1 个元素会成为最大的元素。
 	重新从尾部开始第 1、2 步的操作，除了在这之前头部已经排好的元素。
 	继续对越来越少的数据进行比较、交换操作，直到没有可比较的数据为止，排序完成。
 */
-func BubbleSort(array []int) {
+func BubbleSort1(array []int) {
 	for i := 0; i < len(array); i++ {
-		//
+		// 蠢
 		for j := 0; j < len(array)-1; j++ {
 			if array[i] < array[j] {
 				array[j], array[i] = array[i], array[j]
@@ -19,4 +20,28 @@ func BubbleSort(array []int) {
 		}
 	}
 	fmt.Println(array)
+}
+
+//
+func BubbleSort2(arr []int) []int {
+	l := len(arr)
+	if l <= 1 {
+		return arr
+	}
+	// 只剩一个不需要冒泡
+	for i := 0; i < l; i++ {
+		// 算法优化
+		needChange := false
+		// j零界点
+		for j := 0; j < l-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				needChange = true
+			}
+		}
+		if !needChange {
+			break
+		}
+	}
+	return arr
 }
--- sort/3-bubbleSort.go
+++ sort/3-bubbleSort.go
@@ -7,29 +7,29 @@ import "fmt"
 	每次循环确定一个数字的位置
 */
 
-func BubbleSort(arr []int) []int{
-	l:=len(arr)
-	if l<=1{
+func BubbleSort(arr []int) []int {
+	l := len(arr)
+	if l <= 1 {
 		return arr
 	}
 	// 只剩一个不需要冒泡
-	for i:=0;i<l-1;i++{
+	for i := 0; i < l-1; i++ {
 		// 算法优化
 		needChange := false
-		for j:=0;j<l-i-1;j++{
-			if arr[j]>arr[j+1]{
-				arr[j],arr[j+1] = arr[j+1],arr[j]
+		for j := 0; j < l-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
 				needChange = true
 			}
 		}
-		if !needChange{
+		if !needChange {
 			break
 		}
 	}
 	return arr
 }
 
-func main()  {
-	arr:=[]int{9,2,5,0,6,1,7,8,4,3}
+func main() {
+	arr := []int{9, 2, 5, 0, 6, 1, 7, 8, 4, 3}
 	fmt.Println(BubbleSort(arr))
 }
