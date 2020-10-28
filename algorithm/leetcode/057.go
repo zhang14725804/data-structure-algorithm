5f7d46db6622422aafe86ca9a00faf796ae9f7a2
zhang14725804 <978817036@qq.com>
2020-10-28 14:49:25 +0800

    leetcoding

--- algorithm/leetcode/057.go
+++ algorithm/leetcode/057.go
@@ -1,8 +1,52 @@
 /*
 	插入区间
 
-	给出一个无重叠的 ，按照区间起始端点排序的区间列表。
+	给出一个无重叠的 ，按照区间起始端点【排序】的区间列表。
 	在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
 
-	todo
+	贪心算法（todo）：这题和他么贪心有卵子关系
+
+	贪心算法一般用来解决需要 “找到要做某事的最小数量” 或 “找到在某些情况下适合的最大物品数量” 的问题，且提供的是无序的输入。
+	贪心算法的思想是每一步都选择最佳解决方案，最终获得全局最佳的解决方案。
+
 */
+func insert(intervals [][]int, newInterval []int) [][]int {
+	newStart, newEnd := newInterval[0], newInterval[1]
+	idx := 0
+	n := len(intervals)
+	res := make([][]int, 0)
+	// intervals元素的第一个元素 小于 newInterval[0]，直接添加到结果
+	for idx < n && newStart > intervals[idx][0] {
+		res = append(res, intervals[idx])
+		idx++
+	}
+
+	temp := make([]int, 2)
+	// intervals最后一个元素的第二个元素 小于 newInterval[0]
+	if len(res) == 0 || res[len(res)-1][1] < newStart {
+		res = append(res, newInterval)
+	} else {
+		// intervals最后一个元素的第二个元素 大于 newInterval[0]，合并区间
+		temp = res[len(res)-1]
+		res = res[:len(res)-1]
+		temp[1] = MaxInt(temp[1], newEnd)
+		res = append(res, temp)
+	}
+
+	for idx < n {
+		temp = intervals[idx]
+		idx++
+		start, end := temp[0], temp[1]
+		// res最后一个元素的第二个元素 小于 start
+		if res[len(res)-1][1] < start {
+			res = append(res, temp)
+		} else {
+			// res最后一个元素的第二个元素 大于 start
+			temp = res[len(res)-1]
+			res = res[:len(res)-1]
+			temp[1] = MaxInt(temp[1], end)
+			res = append(res, temp)
+		}
+	}
+	return res
+}
--- algorithm/leetcode/059.go
+++ algorithm/leetcode/059.go
@@ -1,3 +1,39 @@
 /*
-	todo
+	给定一个正整数 n，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
 */
+func generateMatrix(n int) [][]int {
+	// 左右上下
+	l, r, t, b := 0, n-1, 0, n-1
+	res := make([][]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = make([]int, n)
+	}
+	num, end := 1, n*n
+	for num <= end {
+		// 从左到右
+		for i := l; i <= r; i++ {
+			res[t][i] = num
+			num++
+		}
+		t++
+		// 从上到下
+		for i := t; i <= b; i++ {
+			res[i][r] = num
+			num++
+		}
+		r--
+		// 从右到左
+		for i := r; i >= l; i-- {
+			res[b][i] = num
+			num++
+		}
+		b--
+		// 从下到上
+		for i := b; i >= t; i-- {
+			res[i][l] = num
+			num++
+		}
+		l++
+	}
+	return res
+}
