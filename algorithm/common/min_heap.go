48c1ddadb354efbcca9468af1439c9f45226cbf7
zhang14725804 <978817036@qq.com>
2020-11-16 14:18:50 +0800

    二叉堆ing

---
+++ algorithm/common/min_heap.go
@@ -0,0 +1,116 @@
+package main
+
+import "fmt"
+
+//	二叉堆满足堆特性：父节点的键值总是保持固定的序关系于任何一个子节点的键值，且每个节点的左子树和右子树都是一个二叉堆。
+//	当父节点的键值总是大于或等于任何一个子节点的键值时为“最大堆”。当父节点的键值总是小于或等于任何一个子节点的键值时为“最小堆
+// todo：合并二叉堆
+type minHeap struct {
+	heapArray []int
+	size      int
+	maxSize   int
+}
+
+// 创建二叉堆
+func newMinHeap(maxSize int) *minHeap {
+	minHeap := &minHeap{
+		heapArray: []int{},
+		size:      0,
+		maxSize:   maxSize,
+	}
+	return minHeap
+}
+
+// 是否是叶子节点
+func (heap *minHeap) isLeaf(index int) bool {
+	return index >= (heap.size/2) && index <= heap.size
+}
+
+func (heap *minHeap) parentIndex(index int) int {
+	return (index - 1) / 2
+}
+
+func (heap *minHeap) leftChildIndex(index int) int {
+	return 2*index + 1
+}
+func (heap *minHeap) rightChildIndex(index int) int {
+	return 2*index + 2
+}
+
+// 插入二叉堆
+func (heap *minHeap) insert(item int) error {
+	if heap.size >= heap.maxSize {
+		return fmt.Errorf("heap is full!")
+	}
+	// 添加新元素到叶子节点
+	heap.heapArray = append(heap.heapArray, item)
+	heap.size++
+	heap.up(heap.size - 1)
+	return nil
+}
+
+// 对于最小堆，是删除最小值；todo：删除任意位置的元素（使用最后一个叶子节点元素填充被删除的元素，然后调整）
+func (heap *minHeap) remove() int {
+	top := heap.heapArray[0]
+	// 把堆存储的最后那个节点移到填在根节点处。再从上而下调整父节点与它的子节点
+	heap.heapArray[0] = heap.heapArray[heap.size-1]
+	heap.heapArray = heap.heapArray[:heap.size-1]
+	heap.size--
+	heap.down(0)
+	return top
+}
+
+// 上浮 调整堆
+func (heap *minHeap) up(index int) {
+	for heap.heapArray[index] < heap.heapArray[heap.parentIndex(index)] {
+		heap.swap(index, heap.parentIndex(index))
+		index = heap.parentIndex(index)
+	}
+}
+
+// 构造二叉堆（小顶堆）
+func (heap *minHeap) buildMinHeap() {
+	// 最后一个非叶子节点开始调整
+	for i := ((heap.size) / 2); i >= 0; i-- {
+		heap.down(i)
+	}
+}
+
+// 下沉操作(不好懂😅)
+func (heap *minHeap) down(cur int) {
+	if heap.isLeaf(cur) {
+		return
+	}
+	smallest := cur
+	left := heap.leftChildIndex(cur)
+	right := heap.rightChildIndex(cur)
+	// 先左边后右边？（question）
+	if left < heap.size && heap.heapArray[left] < heap.heapArray[smallest] {
+		smallest = left
+	}
+	if right < heap.size && heap.heapArray[right] < heap.heapArray[smallest] {
+		smallest = right
+	}
+	if smallest != cur {
+		heap.swap(cur, smallest)
+		heap.down(smallest)
+	}
+	return
+}
+
+func (heap *minHeap) swap(i, j int) {
+	heap.heapArray[i], heap.heapArray[j] = heap.heapArray[j], heap.heapArray[i]
+}
+
+func main() {
+	inputArray := []int{6, 5, 3, 7, 2, 8}
+	minHeap := newMinHeap(len(inputArray))
+	for i := 0; i < len(inputArray); i++ {
+		minHeap.insert(inputArray[i])
+	}
+	minHeap.buildMinHeap()
+	fmt.Println(minHeap.heapArray)
+	// for i := 0; i < len(inputArray); i++ {
+	// 	fmt.Println(minHeap.remove())
+	// }
+}
--- algorithm/leetcode/base-data-structure/692.go
+++ algorithm/leetcode/base-data-structure/692.go
@@ -1,21 +1,12 @@
 /*
+	692. Top K Frequent Words（找出出现次数最多的K个单词）
+
 	todos：：堆的操作（如何实现一个堆）：
 	（1）查找最大值
 	（2）插入一个数
-	（3）删除一个树
-*/
+	（3）删除一个数
 
-/*
-	692. Top K Frequent Words（找出出现次数最多的K个单词）
-
-	（找出出现次数最多的K个单词）用小根堆来维护出现次数最多的K个单词
-
-	（找出出现次数最少的K个单词）用大根堆来维护出现次数最多的K个单词
-	
-	（todos：：归并排序，快排要看看）
-	
 	["i", "love", "leetcode", "i", "love", "coding"]测试不通过
-	["i", "love", "leetcode", "i", "love", "coding"] 测试通过
 */
 
 func topKFrequent(words []string, k int) []string {
@@ -29,21 +20,23 @@ func topKFrequent(words []string, k int) []string {
 	for key, value := range hash {
 		heap.push(&Node{value, key})
 		// 注意这里是k+1
-		if len(heap.Element) > k+1 {
+		if len(heap.Heap) > k+1 {
 			heap.pop()
 		}
 	}
+	// 构造返回结果
 	res := make([]string, k)
-	// 
-	for i := k-1; i >= 0; i-- {
+	for i := k - 1; i >= 0; i-- {
 		res[i] = heap.pop().second
 	}
 	return res
 }
 
 type minHeap struct {
-	Element []*Node
+	Heap []*Node
 }
+
+// node(频数，单词)（模拟pair）
 type Node struct {
 	first  int
 	second string
@@ -52,7 +45,7 @@ type Node struct {
 /*
 	数组中第一个元素不使用，存放一个小于堆中任何数字的值用于结束循环
 	todos：：为什么要存一个数，而又不使用呢
-*/ 
+*/
 func newMinHeap() *minHeap {
 	heap := &minHeap{}
 	heap.push(&Node{-100000, ""})
@@ -64,14 +57,13 @@ func newMinHeap() *minHeap {
 	todos：：什么是上浮操作
 */
 func (h *minHeap) push(node *Node) {
-	h.Element = append(h.Element, node)
-	n := len(h.Element) - 1
+	h.Heap = append(h.Heap, node)
+	n := len(h.Heap) - 1
 	// 上浮
-	for ; h.Element[n/2].first > node.first; n /= 2 {
-		h.Element[n] = h.Element[n/2]
+	for ; h.Heap[n/2].first > node.first; n /= 2 {
+		h.Heap[n] = h.Heap[n/2]
 	}
-
-	h.Element[n] = node
+	h.Heap[n] = node
 }
 
 /*
@@ -80,33 +72,25 @@ func (h *minHeap) push(node *Node) {
 	todos：：下沉操作
 */
 func (h *minHeap) pop() *Node {
-	if len(h.Element) <= 1 {
+	if len(h.Heap) <= 1 {
 		return nil
 	}
-	last := h.Element[len(h.Element)-1]
+	last := h.Heap[len(h.Heap)-1]
 	// 保存min最小值
-	min := h.Element[1]
+	min := h.Heap[1]
 	var i, child int
-	for i = 1; i*2 < len(h.Element); i = child {
+	for i = 1; i*2 < len(h.Heap); i = child {
 		child = i * 2
-		if child < len(h.Element)-1 && h.Element[child+1].first < h.Element[child].first {
+		if child < len(h.Heap)-1 && h.Heap[child+1].first < h.Heap[child].first {
 			child += 1
 		}
-		if last.first > h.Element[child].first {
-			h.Element[i] = h.Element[child]
+		if last.first > h.Heap[child].first {
+			h.Heap[i] = h.Heap[child]
 		} else {
 			break
 		}
 	}
-	h.Element[i] = last
-	h.Element = h.Element[:len(h.Element)-1]
+	h.Heap[i] = last
+	h.Heap = h.Heap[:len(h.Heap)-1]
 	return min
-}
-
-// 获取最小值
-func (h *minHeap) mix() *Node {
-	if len(h.Element) > 1 {
-		return h.Element[1]
-	}
-	return nil
 }
