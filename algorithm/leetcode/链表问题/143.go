d719c0eb48137b210643ca46bc861932762d8de2
小虎 <978817036@qq.com>
2021-04-20 12:00:35 +0800

    leetcoding

---
+++ algorithm/leetcode/链表问题/143.go
@@ -0,0 +1,106 @@
+/*
+	给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+	将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+	你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+	question 😅😅😅😅😅😅
+*/
+
+/*
+	方法1：把链表存储到线性表中，然后用双指针依次从头尾取元素即可（说的轻松 😅）
+*/
+func reorderList(head *ListNode) {
+	if head == nil {
+		return
+	}
+	list := make([]*ListNode, 0)
+	for head != nil {
+		list = append(list, head)
+		head = head.Next
+	}
+	// 双指针遍历数组，依次改变指针方向（注意指针指向 😅😅😅）
+	start, end := 0, len(list)-1
+	for start < end {
+		list[start].Next = list[end]
+		start++
+		if start == end {
+			break
+		}
+		list[end].Next = list[start]
+		end--
+	}
+	list[start].Next = nil
+}
+
+/*
+	方法2：将链表平均分成两半，将第二个链表逆序，依次连接两个链表（说的轻松 😅）
+*/
+func reorderList(head *ListNode) {
+	if head == nil || head.Next == nil || head.Next.Next == nil {
+		return
+	}
+
+	dummy := new(ListNode)
+	dummy.Next = head
+	// 快慢指针，找到中间节点
+	fast, slow := dummy, dummy
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	// 后半部分
+	tail := slow.Next
+	// 切断前后两段之间的链接
+	slow.Next = nil
+	// 反转后半部分
+	tail = reverseList(tail)
+	// 拼接前后两部分（顺序不能乱 最绕的 😅😅😅😅😅😅😅😅）
+	for tail != nil {
+		tNext := tail.Next    // 缓存
+		tail.Next = head.Next // 链接tail.Next和head.Next
+		head.Next = tail      // 链接tail和head
+		head = tail.Next      // head = head.Next
+		tail = tNext          // tail = tail.Next
+	}
+}
+
+/*
+	方法3：递归（没看懂dfs内部逻辑 😅😅😅）
+*/
+func reorderList3(head *ListNode) {
+	if head == nil || head.Next == nil || head.Next.Next == nil {
+		return
+	}
+
+	n := 0
+	h := head
+	// 计算节点个数
+	for h != nil {
+		h = h.Next
+		n++
+	}
+	dfs(head, n)
+}
+
+func dfs(head *ListNode, n int) *ListNode {
+	// 递归出口
+	if n == 1 {
+		tail := head.Next
+		head.Next = nil
+		return tail
+	}
+
+	if n == 2 {
+		tail := head.Next.Next
+		head.Next.Next = nil
+		return tail
+	}
+
+	tail := dfs(head.Next, n-2)
+	sub := head.Next
+	head.Next = tail
+	outTail := tail.Next
+	tail.Next = sub
+	return outTail
+}
+
--- algorithm/leetcode/链表问题/148.go
+++ algorithm/leetcode/链表问题/148.go
@@ -1,57 +1,61 @@
 /*
-	148. Sort List
+	给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+	进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
 
-	Sort a linked list in O(n log n) time using constant space complexity.
+	😅😅😅😅😅😅
+	question
+	归并排序（MERGE-SORT）是利用归并的思想实现的排序方法，该算法采用经典的分治（divide-and-conquer）策略
+	（分治法将问题分(divide)成一些小的问题然后递归求解，而治(conquer)的阶段则将分的阶段得到的各答案"修补"在一起，即分而治之)
 */
 func sortList(head *ListNode) *ListNode {
-	// 获取链表长度
-	n := 0
-	p := head
-	for p != nil {
-		p = p.Next
-		n++
-	}
-	// 第一个节点有可能变，所以先声明一个虚拟头结点
-	newHead := &ListNode{}
-	newHead.Next = head
+	return mergeSort(head)
+}
 
-	for i := 1; i < n; i *= 2 {
-		cur := newHead
-		for j := 0; j+i < n; j += i * 2 {
-			left := cur.Next
-			right := cur.Next
-			for k := 0; k < i; k++ {
-				right = right.Next
-			}
-			l := 0
-			r := 0
-			for l < i && r < i && right != nil {
-				if left.Val <= right.Val {
-					cur.Next = left
-					cur = left
-					left = left.Next
-					l++
-				} else {
-					cur.Next = right
-					cur = right
-					right = right.Next
-					r++
-				}
-			}
-			for l < i {
-				cur.Next = left
-				cur = left
-				left = left.Next
-				l++
-			}
-			for r < i && right != nil {
-				cur.Next = right
-				cur = right
-				right = right.Next
-				r++
-			}
-			cur.Next = right
+func mergeSort(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	// 😅😅😅 为什么这里需要虚拟头节点（当节点个数是偶数的时候，让 slow 刚好指向前边一半节点的最后一个节点）
+	dummy := new(ListNode)
+	dummy.Next = head
+	fast, slow := dummy, dummy
+	// 快慢指针找到中点
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	tail := slow.Next
+	// 😅😅😅 切断两段之间的链接
+	slow.Next = nil
+	// 前半部分
+	head = mergeSort(head)
+	// 后半部分
+	tail = mergeSort(tail)
+	return merge(head, tail)
+}
+
+func merge(head1, head2 *ListNode) *ListNode {
+	// 😅😅😅 又是虚拟头节点
+	dummy := new(ListNode)
+	// 😅😅😅 这个节点是专门用来遍历的
+	tail := dummy
+	for head1 != nil && head2 != nil {
+		if head1.Val < head2.Val {
+			tail.Next = head1
+			tail = tail.Next
+			head1 = head1.Next
+		} else {
+			tail.Next = head2
+			tail = tail.Next
+			head2 = head2.Next
 		}
 	}
-	return newHead.Next
+	if head1 != nil {
+		tail.Next = head1
+	}
+	if head2 != nil {
+		tail.Next = head2
+	}
+	return dummy.Next
 }
