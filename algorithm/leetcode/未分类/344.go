94cad9f93ba987ff2120d285602811e77a06b39e
张小虎-架构 <zhangxiaohu@xiaolian.link>
2023-02-21 16:03:51 +0800

    344

--- algorithm/leetcode/未分类/344.go
+++ algorithm/leetcode/未分类/344.go
@@ -1,11 +1,8 @@
-/*
-	编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
-	不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
-	你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
-*/
 
 /*
-	方法1：// 双指针
+	方法一：双指针
+	start向右走，end向左走
+	依次调换位置，直到二者重合
 */
 func reverseString(s []byte) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
@@ -14,7 +11,7 @@ func reverseString(s []byte) {
 }
 
 /*
-	方法2：递归
+	方法二：递归
 */
 func reverseString(s []byte) {
 	res := make([]byte, 0)
