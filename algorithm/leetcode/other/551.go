46d92e6b07f837dc1e89c8e003b5aad3cf078b22
张小虎-架构 <zhangxiaohu@xiaolian.link>
2023-02-23 14:21:05 +0800

    551

--- algorithm/leetcode/other/551.go
+++ algorithm/leetcode/other/551.go
@@ -3,4 +3,54 @@
 		遇到A，count++，判断是否大于2，满足条件return false
 		遇到L，for下一个是否是L，count++，判断是否大于等于3，满足条件return false
 		否则return true
+		我的蠢代码 😅
 */
+func checkRecord(s string) bool {
+	acount := 0
+	for i := 0; i < len(s); i++ {
+		// A 没什么好说的
+		if s[i] == 'A' {
+			acount++
+			if acount > 1 {
+				return false
+			}
+		}
+		// 判断是否连续三次迟到
+		lcount := 0
+		for i < len(s) && s[i] == 'L' {
+			lcount++
+			i++
+		}
+		if lcount > 2 {
+			return false
+		} else if lcount > 0 {
+			// 注意这里要退回1位 "ALLAPPL"
+			i--
+		}
+
+	}
+	return true
+}
+
+func checkRecord(s string) bool {
+	acount := 0
+	late := 0
+	for _, c := range s {
+		if c == 'A' {
+			acount++
+			if acount > 1 {
+				return false
+			}
+		}
+		if c == 'L' {
+			late++
+			if late > 2 {
+				return false
+			}
+		} else {
+			// 不是L切没有满足犯错条件
+			late = 0
+		}
+	}
+	return true
+}
