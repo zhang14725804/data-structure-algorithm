6211c8b5321dc270c7403f5691e4c2a59c971fd0
张小虎-架构 <zhangxiaohu@xiaolian.link>
2023-03-04 19:53:41 +0800

    反转字符串

--- algorithm/leetcode/字符串处理/03-151.go
+++ algorithm/leetcode/字符串处理/03-151.go
@@ -1,88 +1,60 @@
 /*
-	给定一个字符串，逐个翻转字符串中的每个单词。
+	1. k记录字符串最终长度
+	2. 遍历字符串
+	3. 反转每个单词
+	4. 再每个单词后添加空格
+	5. 确定每个字符最终位置
+	6. 去掉多余的部分
+	7. 反转整个字符串
 
-	输入: "the sky is blue"
-	输出: "blue is sky the"
-
-	说明：
-
-	无空格字符构成一个单词。
-	输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
-	如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
-
-	（1）先反转每个单词blue --> eulb
-	（2）反转整个字符串
-	（3）空格
-	todo：这种方法没做出来，从判断k开始有问题
 */
 func reverseWords(str string) string {
-	s:=[]rune(str)
+	bstr := []rune(str)
 	// 最终字符串长度
-	k:=0
-	sLen := len(s)
+	k := 0
+	sLen := len(str)
 	// 遍历字符串
-	for i:=0;i<sLen;i++{
+	for i := 0; i < sLen; i++ {
 		// 过滤连续空格
-		for i<sLen && string(s[i]) == " " {
+		for i < sLen && bstr[i] == ' ' {
 			i++
 		}
-		if i==sLen{
+		if i == sLen {
 			break
 		}
-		
-		j:=i
-		// 连续的非空格
-		for j<sLen && string(s[j])!=" " {
+
+		// 反转单词
+		j := i
+		for j < sLen && bstr[j] != ' ' {
 			j++
 		}
-		// 反转单词
-		reverse(s[i:j])
+		reverse(bstr[i:j])
 
-		// 
-		if k>0{
-			// 不能写s[k++]
+		// 增加一个空格
+		if k > 0 {
+			bstr[k] = ' '
 			k++
-			s[k] = ' '
 		}
-		for i < j-1{
+
+		// 确定每个单子最终位置
+		for i < j {
+			bstr[k] = bstr[i]
 			k++
 			i++
-			s[k] = s[i]
 		}
 	}
-	// 反转字符串
-	reverse(s[0:sLen])
-	return string(s)
-	
+
+	// 去掉多余的部分
+	res := bstr[:k]
+	// 反转整个字符串
+	reverse(res)
+
+	return string(res)
 }
+
 func reverse(a []rune) []rune {
-    for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
-        a[i], a[j] = a[j], a[i]
-    }
-    return a
-}
-
-
-// 这种方法用了不少库函数
-func reverseWords(s string) string {
-	// 根据空格拆分字符串
-	arr := strings.Split(s, " ")
-	res := []string{}
-	// 过滤多余空格
-	for i := 0; i < len(arr); i++ {
-		if arr[i] != " " {
-			res = append(res, arr[i])
-		}
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
 	}
-	// 交换位置
-	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
-		res[i], res[j] = res[j], res[i]
-	}
-	/*
-		fmt.Sprint(res) 将res数组连同[]转化为字符串类型
-		strings.Trim(fmt.Sprint(res), "[]") 去除[]
-		func Replace(s, old, new string, n int) string 返回将s中前n个不重叠old子串都替换为new的新字符串，如果n<0会替换所有old子串。
-	*/
-	return strings.Replace(strings.Trim(fmt.Sprint(res), "[]"), " ", " ", -1)
-
+	return a
 }
