4eb62450f3e376f4626975238c2f0c4575f2e827
张小虎-架构 <zhangxiaohu@xiaolian.link>
2023-03-08 14:01:54 +0800

    字符串问题

--- algorithm/leetcode/week6/026.go
+++ algorithm/leetcode/week6/026.go
@@ -1,11 +1,10 @@
 /*
-	给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
-	不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
-
-	原地交换位置问题
+	方法1：双指针算法，快慢指针
+		1. slow只走不重复的数据
+		2. fast每次都走
+		3. 遇到重复的数据，slow跳过
+		4. 遇到不重复的数据，slow先前进一步，并更新nums[slow]的值
 */
-
-// 方法1：双指针算法，快慢指针
 func removeDuplicates(nums []int) int {
 	if len(nums) == 0 {
 		return 0
@@ -14,8 +13,8 @@ func removeDuplicates(nums []int) int {
 	slow, fast := 0, 0
 	// fast从前向后遍历
 	for fast < len(nums) {
-		// 判断是否和上一个数相同
 		if nums[slow] != nums[fast] {
+			// 😅 slow需要先走，后更新 nums[slow]
 			slow++
 			nums[slow] = nums[fast]
 		}
