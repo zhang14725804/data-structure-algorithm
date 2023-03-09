3c35723dd56e409cff88677efeffcbe17f012eb7
张小虎-架构 <zhangxiaohu@xiaolian.link>
2023-03-09 09:33:35 +0800

    一个小时做了一道😅

--- algorithm/leetcode/滑动窗口/076.go
+++ algorithm/leetcode/滑动窗口/076.go
@@ -1,15 +1,15 @@
 /*
 	方法1：滑动窗口
-	如何快速判断当前字串是否包含t中所有字母，hash或数组
-	TODO 😅😅😅
+		1. 统计t中字符的种类和每个字符出现的次数
+		2. 双指针left,right
+		3. 移动right，检查当前元素是否在t中
+		4. 若存在，更新窗口内容，检查是否满足t元素种类和个数
+		5. 若满足要求，右移left收缩窗口，收缩的过程中更新窗口内容
 */
 func minWindow(s string, t string) string {
-	// 需要满足的字符及其个数组成的map
 	need := make(map[byte]int, 0)
-	window := make(map[byte]int, 0)
 	// needSize 要满足的元素个数
-	// valid 当前已满足的元素个数
-	needSize, left, right, valid := 0, 0, 0, 0
+	needSize := 0
 	for i := 0; i < len(t); i++ {
 		c := t[i]
 		need[c]++
@@ -19,20 +19,24 @@ func minWindow(s string, t string) string {
 		}
 	}
 
+	// valid 当前已满足的元素个数
+	left, right, valid := 0, 0, 0, 0
+	window := make(map[byte]int, 0)
 	var res string
 	for right < len(s) {
 		// 将要入窗口的字符
 		c := s[right]
 		// 移动右边指针
 		right++
-		// 进行窗口内数据的一系列更新
+		// 若当前字符存在于t中
 		if _, ok := need[c]; ok {
 			window[c]++
 			if window[c] == need[c] {
 				valid++
 			}
 		}
-		// 判断左侧边界是否需要收缩
+
+		// 😅 持续收缩，判断左侧边界是否需要收缩
 		for valid == needSize {
 			// 更新最小覆盖字串
 			if len(res) == 0 || len(res) > right-left {
@@ -42,7 +46,7 @@ func minWindow(s string, t string) string {
 			d := s[left]
 			// 向左移动左边指针
 			left++
-			// 进行窗口内数据的一系列更新
+			// 若当前字符存在于t中
 			if _, ok := need[d]; ok {
 				if window[d] == need[d] {
 					valid--
@@ -56,13 +60,12 @@ func minWindow(s string, t string) string {
 
 /*
 	方法2：滑动窗口（优化版）(question) 😅😅😅
-	如何快速判断当前字串是否包含t中所有字母，hash或数组
+		TODO
 */
 func minWindow2(s1 string, t1 string) string {
-	// need始终记录着当前滑动窗口下，我们还需要的元素数量
-	// 数量为负的就是不必要的元素，而数量为0表示刚刚好
+	// need始终记录着当前滑动窗口下，我们还需要的元素种类
 	need := make(map[byte]int, 0)
-	// 记录所需元素的总数量（不同字母的种类）
+	// 记录所需元素种类
 	cnt := 0
 	// 字符串遍历：range遍历的结果是rune，for便利的结果是byte
 	for i := 0; i < len(t1); i++ {
@@ -75,6 +78,7 @@ func minWindow2(s1 string, t1 string) string {
 
 	var res string
 
+	// 数量为负的就是不必要的元素，而数量为1表示刚刚好
 	// 从前往后维护窗口。i先走，j在后
 	for i, j, count := 0, 0, 0; i < len(s1); i++ {
 		// (question)没看懂；为什么count只加不减，need中有加有减，count为什么就特殊了😅
