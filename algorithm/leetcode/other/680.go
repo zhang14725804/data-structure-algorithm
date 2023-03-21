2cd1b55cf03a5643ebcaa62f9e3766f6df537b25
张小虎-架构 <zhangxiaohu@xiaolian.link>
2023-03-21 09:08:42 +0800

    双指针 & findNthDigit

--- algorithm/leetcode/other/680.go
+++ algorithm/leetcode/other/680.go
@@ -1,25 +1,27 @@
 /*
-	给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
-
-	思路：双指针，一个从头开始遍历，一个从尾部开始遍历，遇到不相同的情况，分两种情况，如果都不满足返回false
+	思路：双指针
+	start从头开始遍历，end从尾部开始遍历，遇到不相同的情况：
+	1. 跳过start，和end继续
+	2. 跳过end，和start继续
+	如果都不满足返回false
 */
 
 func validPalindrome(s string) bool {
-	l,r := 0,len(s)-1
-	for l<r{
-		// 不同的地方这么处理
-		if s[l] != s[r]{
-			return isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1)
+	l, r := 0, len(s)-1
+	for l < r {
+		// 😅 不同的地方这么处理
+		if s[l] != s[r] {
+			return isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1)
 		}
 		l++
 		r--
 	}
-    return true
+	return true
 }
 
-func isPalindrome(s string,l,r int) bool {
-	for l<r{
-		if s[l] != s[r]{
+func isPalindrome(s string, l, r int) bool {
+	for l < r {
+		if s[l] != s[r] {
 			return false
 		}
 		l++
