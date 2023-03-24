be585803a8d99d16a53caf528294e869c1e2c0a5
张小虎-架构 <zhangxiaohu@xiaolian.link>
2023-03-24 09:22:00 +0800

    TODO

---
+++ algorithm/leetcode/other/393.go
@@ -0,0 +1,4 @@
+/*
+	没读懂题意 😅
+	https://leetcode.cn/problems/utf-8-validation/
+*/
--- algorithm/leetcode/other/560.go
+++ algorithm/leetcode/other/560.go
@@ -1,15 +1,9 @@
 /*
-	560. Subarray Sum Equals K
-
-	把这个题改成多少和小于等于K：：平衡树的基础上二分
-*/
-
-/*
-	前缀和
+	前缀和 为啥我想不到 😅
 */
 func subarraySum1(nums []int, k int) int {
 	size := len(nums)
-	// 前缀和数组
+	// 😅 前缀和数组，为什么要【size+1】
 	preSum := make([]int, size+1)
 	preSum[0] = 0
 	// 构造前缀和
@@ -19,7 +13,7 @@ func subarraySum1(nums []int, k int) int {
 	ans := 0
 	for i := 1; i <= size; i++ {
 		for j := 0; j < i; j++ {
-			// sum of nums[j..i-1]
+			// 😅😅😅 nums[j..i-1]区间和
 			if preSum[i]-preSum[j] == k {
 				ans++
 			}
@@ -30,11 +24,12 @@ func subarraySum1(nums []int, k int) int {
 
 /*
 	前缀和和hash（边界问题）
+	😅😅😅 不懂
 */
 func subarraySum(nums []int, k int) int {
-	// 以和为键，出现次数为对应的值
+	// key：前缀和，value：key 对应的前缀和的个数
 	hash := make(map[int]int, 0)
-	// 注意边界问题
+	// 😅 对于下标为 0 的元素，前缀和为 0，个数为 1
 	hash[0] = 1
 	res := 0
 	//  [0..i][0..i] 里所有数的和
@@ -42,8 +37,9 @@ func subarraySum(nums []int, k int) int {
 	for i := 0; i < len(nums); i++ {
 		// 前缀和
 		preSum += nums[i]
-		// question 不理解
+		// 先获得前缀和为 preSum - k 的个数，加到计数变量里
 		res += hash[preSum-k]
+		// 然后维护 preSum 的定义
 		hash[preSum] += 1
 	}
 	return res
